dd4ffe35d016eba92750b2be38b90acdac5925a4
HaKaiiii <paaris12345@gmail.com>
2023-01-01 02:48:14 +0500

    Added knight movement

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -10,3 +10,39 @@ Knight::~Knight()
 {
 	SDL_DestroyTexture(this->GetTexture());
 }
+
+void Knight::RenderPossibleMoves(SDL_Renderer* Renderer)
+{
+
+	Piece** const boardPosition = Chess::GetBoardPos();
+
+	int x = static_cast<int>(this->GetPieceX());
+	int y = static_cast<int>(this->GetPieceY());
+
+
+	if((x + 2 < 8 && y + 1 < 8) && (!boardPosition[(x + 2) + ((y + 1) * 8)] || (boardPosition[(x + 2) + ((y + 1) * 8)] && boardPosition[(x + 2) + ((y + 1) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x + 2) + ((y + 1) * 8));
+
+	if ((x + 2 < 8 && y - 1 > -1) && (!boardPosition[(x + 2) + ((y - 1) * 8)] || (boardPosition[(x + 2) + ((y - 1) * 8)] && boardPosition[(x + 2) + ((y - 1) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x + 2) + ((y - 1) * 8));
+
+	if ((x - 2 > -1 && y + 1 < 8) && (!boardPosition[(x - 2) + ((y + 1) * 8)] || (boardPosition[(x - 2) + ((y + 1) * 8)] && boardPosition[(x - 2) + ((y + 1) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x - 2) + ((y + 1) * 8));
+
+	if ((x - 2 > -1 && y - 1 > -1) && (!boardPosition[(x - 2) + ((y - 1) * 8)] || (boardPosition[(x - 2) + ((y - 1) * 8)] && boardPosition[(x - 2) + ((y - 1) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x - 2) + ((y - 1) * 8));
+
+	if ((x + 1 < 8 && y - 2 > -1) && (!boardPosition[(x + 1) + ((y - 2) * 8)] || (boardPosition[(x + 1) + ((y - 2) * 8)] && boardPosition[(x + 1) + ((y - 2) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x +1) + ((y - 2) * 8));
+
+	if ((x + 1 < 8 && y + 2 < 8) && (!boardPosition[(x + 1) + ((y + 2) * 8)] || (boardPosition[(x + 1) + ((y + 2) * 8)] && boardPosition[(x + 1) + ((y + 2) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x + 1) + ((y + 2) * 8));
+
+	if ((x - 1 > -1 && y -2 > -1) && (!boardPosition[(x - 1) + ((y - 2) * 8)] || (boardPosition[(x - 1) + ((y - 2) * 8)] && boardPosition[(x - 1) + ((y - 2) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x - 1) + ((y - 2) * 8));
+
+	if ((x - 1 > -1 && y + 2 < 8) && (!boardPosition[(x - 1) + ((y + 2) * 8)] || (boardPosition[(x - 1) + ((y + 2) * 8)] && boardPosition[(x - 1) + ((y + 2) * 8)]->GetPieceTeam() != this->GetPieceTeam())))
+		this->PossibleMovesVector().push_back((x - 1) + ((y + 2) * 8));
+
+	this->RenderPossMovesBlock(Renderer);
+}
